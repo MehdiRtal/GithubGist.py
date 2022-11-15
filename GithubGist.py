f5f584b5a7db992e35093847f27ec7e680eef25b
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-11-15 10:15:36 +0000

    Update GithubGist.py

--- GithubGist.py
+++ GithubGist.py
@@ -2,22 +2,22 @@ import requests
 
 class GitHubGist:
     def __init__(self, token):
-        self.token = token
+        self.headers = {"auth": token}
 
     def get_gists(self):
-        r = requests.get("https://api.github.com/gists", headers={"auth": self.token})
+        r = requests.get("https://api.github.com/gists", headers=self.headers)
         if r.status_code != 200:
             raise Exception(r.text)
         return r.json()
 
     def get_gist(self, id):
-        r = requests.get(f"https://api.github.com/gists/{id}", headers={"auth": self.token})
+        r = requests.get(f"https://api.github.com/gists/{id}", headers=self.headers)
         if r.status_code != 200:
             raise Exception(r.text)
         return r.json()
 
     def update_gist(self, id, filename, content):
-        r = requests.patch(f"https://api.github.com/gists/{id}", headers={"auth": self.token}, json={"files": {filename: {"content": content}}})
+        r = requests.patch(f"https://api.github.com/gists/{id}", headers=self.headers, json={"files": {filename: {"content": content}}})
         if r.status_code != 200:
             raise Exception(r.text)
     
